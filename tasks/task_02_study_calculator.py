09266474b740535cccc285fffd0e72be6f85c193
snowkill32 <2007obukhov@gmail.com>
2026-05-15 12:14:51 +0300

    changed output in task_02_study_calculator.py

--- tasks/task_02_study_calculator.py
+++ tasks/task_02_study_calculator.py
@@ -2,7 +2,8 @@ a = float(input('Сколько часов в день ты учишься в б
 b = float(input('Сколько часов в день ты учишься в выходные? '))
 week_hours = a*5 + b*2
 month_hours = week_hours * 4
-if week_hours < 7: print(f'{week_hours} Slow pace')
-elif 7 < week_hours < 12: print(f'{week_hours} Normal pace')
-else: print(f'{week_hours} Strong pace')
+print(f'Weekly hours: {week_hours}')
+if week_hours < 7: print('Pace: slow pace')
+elif 7 < week_hours < 12: print('Pace: normal pace')
+else: print('Pace: strong pace')
 print(f'Monthly hours: {month_hours}')
