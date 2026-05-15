4f3a3d9e3325dc0bc7c4cb22a04fe2b7d1321b4a
snowkill32 <2007obukhov@gmail.com>
2026-05-15 10:52:49 +0300

    Add first Python basics task

---
+++ tasks/task_01_basics.py
@@ -0,0 +1,11 @@
+name = "snowkill32"
+weekday_hours = 1.5
+weekend_hours = 3
+
+weekly_hours = weekday_hours * 5 + weekend_hours * 2
+
+print("Стартовый профиль")
+print("Имя:", name)
+print("Учёба в будни:", weekday_hours, "часа")
+print("Учёба в выходные:", weekend_hours, "часа")
+print("Всего часов в неделю:", weekly_hours)
