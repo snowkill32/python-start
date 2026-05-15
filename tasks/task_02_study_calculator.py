d03468a79fee489f27c28459da5064b290358da4
snowkill32 <2007obukhov@gmail.com>
2026-05-15 12:23:44 +0300

    Rebuilded task_02_study_calculator.py in python-start

--- tasks/task_02_study_calculator.py
+++ tasks/task_02_study_calculator.py
@@ -1,9 +1,16 @@
-a = float(input('Сколько часов в день ты учишься в будни? '))
-b = float(input('Сколько часов в день ты учишься в выходные? '))
-week_hours = a*5 + b*2
+weekday_hours = float(input("Enter weekday study hours: "))
+weekend_hours = float(input("Enter weekend study hours: "))
+
+week_hours = weekday_hours * 5 + weekend_hours * 2
 month_hours = week_hours * 4
-print(f'Weekly hours: {week_hours}')
-if week_hours < 7: print('Pace: slow pace')
-elif 7 < week_hours < 12: print('Pace: normal pace')
-else: print('Pace: strong pace')
-print(f'Monthly hours: {month_hours}')
+
+print(f"Weekly hours: {week_hours}")
+
+if week_hours < 7:
+    print("Pace: slow pace")
+elif week_hours < 12:
+    print("Pace: normal pace")
+else:
+    print("Pace: strong pace")
+
+print(f"Monthly hours: {month_hours}")
