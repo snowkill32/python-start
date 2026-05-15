e151be78b79c43affc9cf284702e6b68e0c6bd4a
snowkill32 <2007obukhov@gmail.com>
2026-05-15 13:03:34 +0300

    Rebuilded task_03_list_practice.py into python-start

--- tasks/task_03_list_practice.py
+++ tasks/task_03_list_practice.py
@@ -1,4 +1,4 @@
-task = ['Learn Git basics', 'Practice Python input', 'Push code to GitHub']
+tasks = ['Learn Git basics', 'Practice Python input', 'Push code to GitHub']
 print("My study tasks: ")
-for index, task in enumerate(task, start = 1):
+for index, task in enumerate(tasks, start = 1):
     print(f"{index}. {task}")
