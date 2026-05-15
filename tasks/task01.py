5bc8acfa589366fcc6f82c4fc4df8a92e2e54293
snowkill32 <2007obukhov@gmail.com>
2026-05-15 11:45:34 +0300

    Add task01 practice file

---
+++ tasks/task01.py
@@ -0,0 +1,9 @@
+name = "snowkill32"
+weekday_hours = 1.5
+weekend_hours = 3
+weakly_hours = (weekday_hours * 5) + (weekend_hours * 2)
+print("Start profile")
+print(f"Name: {name}")
+print(f"Weekday study hours: {weekday_hours}")
+print(f"Weekend study hours: {weekend_hours}")
+print(f"Total hours per week: {weakly_hours}")
