71cd7e5c1947d5a224d63c19317ac96d818c0f89
snowkill32 <2007obukhov@gmail.com>
2026-05-16 12:12:19 +0300

    Added new mini project

---
+++ tasks/task_05_task_status.py
@@ -0,0 +1,8 @@
+tasks = [
+    {"title": "Learn dict", "status": "active"},
+    {"title": "Learn lists", "status": "active"},
+    {"title": "Learn loops", "status": "active"}
+]
+print("My tasks: ")
+for index, task in enumerate(tasks, start = 1):
+    print(f'{index}. {task["title"]} | {task["status"]}')
